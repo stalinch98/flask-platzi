08946afd1f0d369bd42b759b81980ea3f18fa0ff
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-05 17:50:25 -0500

    Templates with Jinja2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -15,4 +15,4 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return 'Hello World from Flask!, your IP is {}'.format(user_ip)
+    return render_template('hello.html', user_ip=user_ip)
