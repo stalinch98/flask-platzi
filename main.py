8362c33dffbccc229c05fb65f1d4dbdd25da65f7
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-07 17:11:38 -0500

    404 error

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 alls = ['All 1', 'All 2', 'All 3']
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
