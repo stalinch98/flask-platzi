ea9565e822a611b32d913eab9a7f0b91969b08e6
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-10 12:27:04 -0500

    Flask wtf

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+alls = ['Buy coffe', 'Send purchase request', 'Deliver video to producer']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Send')
 
 
 @app.errorhandler(404)
@@ -32,9 +41,11 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'alls': alls,
+        'login_form': login_form,
     }
 
     return render_template('hello.html', **context)
