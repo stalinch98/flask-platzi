4fa88b4d2633b909bfc7c35b3b4538b8b3014af7
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-10 11:30:13 -0500

    Flask bootstrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 alls = ['All 1', 'All 2', 'All 3']
 
