a2d508d290bdd38a672ded4c850e1d79bc114cf8
Stalin Chiguano <stalin_dany98@hotmail.com>
2020-09-10 12:07:11 -0500

    Save session and save the user's IP in the session

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-alls = ['All 1', 'All 2', 'All 3']
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.errorhandler(404)
@@ -12,20 +14,27 @@ def not_found(error):
     return render_template('404.html', error=error)
 
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
+
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
-        'alls': alls
+        'todos': todos,
     }
+
     return render_template('hello.html', **context)
